91b21735ce182df97059f8bd12e2323e1e13e900
andreamsilu <msiluandrew2020@gmail.com>
2024-11-28 07:52:55 +0300

    fixed

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Cart from '../components/Cart.vue';
-import Checkout from '../components/Checkout.vue';
+// import Checkout from '../components/Checkout.vue';
 
 const routes = [
   { path: '/', component: Home },
   { path: '/cart', component: Cart },
-  { path: '/checkout', component: Checkout },
+  // { path: '/checkout', component: Checkout },
 ];
 
 const router = createRouter({
